2038306c9e18fd925a42124bc18e98d3c8384727
Raí Siqueira <rai93siqueira@gmail.com>
2018-01-17 21:53:52 -0300

    lint file

--- app/initialize.js
+++ app/initialize.js
@@ -3,6 +3,7 @@ import React from 'react';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
 import reducers from './reducers';
+
 // import Counter from './components/counter';
 import Home from './components/home';
 import * as SDKInitializer from './libs/SDKInitializer';
@@ -10,9 +11,9 @@ import * as SDKInitializer from './libs/SDKInitializer';
 const createStoreWithMiddleware = applyMiddleware()(createStore);
 
 const App = () =>
-	<Provider store={createStoreWithMiddleware(reducers)}>
-		<Home />
-	</Provider>;
+  <Provider store={createStoreWithMiddleware(reducers)}>
+    <Home />
+  </Provider>;
 
 ReactDOM.render(<App />, document.querySelector('#app'));
 
