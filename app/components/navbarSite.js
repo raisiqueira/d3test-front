150c24088dda18a9712f7dfe77338d69e57434c3
Raí Siqueira <rai93siqueira@gmail.com>
2018-01-18 22:12:45 -0300

    navbar header

---
+++ app/components/navbarSite.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+
+class Navbar extends Component {
+
+  render () {
+    return (
+    <div>
+      <nav className="navbar">
+        <div className="container">
+        <h3 className="pull-left">EXP_</h3>
+
+        <h3 className="pull-right">Karol com 5K_</h3>
+        </div>
+      </nav>
+    </div>
+    );
+  };
+};
+
+export default Navbar;
