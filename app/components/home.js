e2fb5f456bd0d1beac35637ae835c2c04a58fbe5
Raí Siqueira <rai93siqueira@gmail.com>
2018-01-23 12:32:16 -0300

    add components to home

--- app/components/home.js
+++ app/components/home.js
@@ -1,8 +1,18 @@
 import React from 'react';
 
+import Navbar from './navbarSite';
+import PageTitle from './pageTitle';
+import Steps from './steps';
+import Forms from './forms';
+
 const Home = props => (
   <div>
-    <h1>Centauro EXP_</h1>
+    <Navbar />
+    <div className="container">
+      <PageTitle pageTitle="Cadastro" />
+    </div>
+    <Steps />
+    <Forms />
   </div>
 );
 
