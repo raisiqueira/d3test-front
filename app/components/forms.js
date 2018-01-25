289780b91bf0c31c87d58331df9fc0e1b50e6b27
Raí Siqueira <rai93siqueira@gmail.com>
2018-01-25 00:17:20 -0300

    add validation for input on the form

--- app/components/forms.js
+++ app/components/forms.js
@@ -19,6 +19,10 @@ class Forms extends Component {
       ccCVC: '',
       ccFocused: '' // for React Credit cards component
     };
+
+    this.handleCardForm = this.handleCardForm.bind(this);
+    this.handleAddressType = this.handleAddressType.bind(this);
+    this.submitForm = this.submitForm.bind(this);
   }
 
   handleCardForm(event, input, focused) {
@@ -28,177 +32,247 @@ class Forms extends Component {
     });
   }
 
+  handleAddressType(event, type) {
+    this.setState({
+      addressType: type
+    });
+  }
+
+  submitForm(event) {
+    let submitErr = false;
+    let addrFields = ['addressAdr', 'addressNum', 'addressUF', 'addressCity'];
+    let reqFields = ['ccNum', 'ccName', 'ccDtMonth', 'ccDtYear', 'ccCVC', 'addressCep'];
+
+    // validate required fields
+    reqFields.map(field => {
+      if (this.state[field] === '') {
+        submitErr = true;
+      }
+    });
+
+    // validate address type and address fields
+    if (this.state.addressType === 'diferent') {
+      addrFields.map(field => {
+        if (this.state[field] === '') {
+          submitErr = true;
+        }
+      });
+    }
+
+    this.props.submitFormError(submitErr);
+  }
+
   render() {
     return (
-      <section className="container main">
-        <div className="main-form col-lg-9">
-          <h3>Pagamento</h3>
-        <div className="form-wrapper">
-        <h4 className="form-title">Endereço de cobrança</h4>
-          <button className="btn form-btn">É o mesmo da Entrega</button>
-          <button className="btn form-btn form-btn-active">É diferente da Entrega</button>
-        </div>
+      <div className='main-form col-lg-9'>
+        <h3 className='form-title'>Pagamento</h3>
 
-        <div className="form-wrapper">
-        <h4 className="form-title">Qual o seu CEP de cobrança?</h4>
-        <div className="form-items">
-        <label className="form-label">
-            CEP
-            <InputMask className="form-input" mask="99999-999" />
-            <span className="form-helper">Não sei meu CEP</span>
-        </label>
-        </div>
-        </div>
+        <div className='form-wrapper'>
+          <h4 className='form-title'>Endereço de cobrança</h4>
+          <button className={`btn form-btn ${this.state.addressType === `same` ? `form-btn-active` : ``}`}
+            onClick={(event) => this.handleAddressType(event, 'same')}
+          >
+            É o mesmo da Entrega
+          </button>
+          <button className={`btn form-btn ${this.state.addressType === `diferent` ? `form-btn-active` : ``}`}
+            onClick={(event) => this.handleAddressType(event, 'diferent')}
+          >
+            É diferente da Entrega
+          </button>
+        </div>{/*form wrapper*/}
 
-        <div className="form-wrapper">
-        <h4 className="form-title">Confirme seu endereço de cobrança</h4>
-        <div className="form-items">
-        <label className="form-label">
-            Logradouro
-            <input className="form-input form-input-addr" type="text"></input>
-        </label>
-        </div>
-        <div className="form-items">
-        <label className="form-label">
-            Número
-            <input className="form-input form-input-addr-num" type="text"></input>
-        </label>
-        </div>
-
-        <div className="form-items">
-        <label className="form-label">
-            Complemento
-            <input className="form-input form-input-compl" type="text"></input>
-        </label>
-        </div>
-
-        <div className="form-items">
-        <label className="form-label">
-            UF
-            <div className="form-select form-select-uf">
-            <select>
-              <option>--</option>
-              <option>BA</option>
-              <option>PE</option>
-              <option>SC</option>
-              <option>SP</option>
-            </select>
-            </div>
-        </label>
-        </div>
-        </div>
-
-        <div className="form-wrapper">
-        <div className="form-items">
-          <label className="form-label">
-            Cidade
-            <div className="form-select form-select-city">
-            <select>
-              <option>--</option>
-              <option>Custódia</option>
-              <option>Garanhuns</option>
-              <option>Recife</option>
-            </select>
-            </div>
-         </label>
-         </div>
-        </div>
-
-        <div className="form-wrapper">
-        <h4 className="form-title">Dados do cartão de crédito</h4>
-            <ReactCreditCards
-              number={this.state.ccNum}
-              name={this.state.ccName}
-              expiry={`${this.state.ccDtMonth}/${this.state.ccDtYear}`}
-              cvc={this.state.ccCVC}
-              focused={this.state.ccFocused}
-            />
-        </div>
-
-        <div className="form-wrapper">
-        <div className="form-items">
-        <label className="form-label">
-          Número
-          <InputMask
-            className="form-input form-input-cc-num"
-            mask='9999 9999 9999 9999'
-            maskChar=''
-            onChange={(e) => this.handleCardForm(e, 'ccNum', 'number')}
-            value={this.state.ccNumber}
-          />
-        </label>
-        </div>
-
-        <div className="form-items">
-        <label className="form-label">
-          Nome
-          <InputMask
-            className="form-input form-input-cc-name"
-            onChange={(e) => this.handleCardForm(e, 'ccName', 'name')}
-            value={this.state.ccNumber}
-          />
-        </label>
-        </div>
-        </div>
-
-        <div className="form-wrapper">
-          <div className="form-items">
-          <label className="form-label">
-            Validade
-            <div className="form-select form-select-uf">
-            <select onChange={(e) => this.handleCardForm(e, 'ccDtMonth', 'expiry')}>
-              <option>Mês</option>
-              <option>01</option>
-              <option>02</option>
-              <option>03</option>
-              <option>04</option>
-            </select>
-            </div>
-            <div className="form-select form-select-uf">
-            <select onChange={(e) => this.handleCardForm(e, 'ccDtYear', 'expiry')}>
-              <option>Ano</option>
-              <option>18</option>
-              <option>19</option>
-              <option>20</option>
-            </select>
-            </div>
-         </label>
-         </div>
-
-         <div className="form-items">
-         <label className="form-label">
-         Código de segurança
-         <InputMask
-           className='form-input form-input-cc-cvc'
-           mask='999'
-           maskChar=''
-           onChange={(e) => this.handleCardForm(e, 'ccCVC', 'cvc')}
-           value={this.state.ccCVC}
-         />
-         </label>
-         </div>
-
-        </div>
-        <div className="form-wrapper">
-        <h4 className="form-title">Valor e parcelamento</h4>
-
-        <div className="form-items">
-        <label className="form-label">
-        <div className="form-select form-select-cc-installments">
-          <select>
-            <option>5x de R$132,00</option>
-            <option>4x de R$165,00</option>
-            <option>3x de R$220,00</option>
-            <option>2x de R$330,00</option>
-            <option>1x de R$660,00</option>
-          </select>
+        <div className='form-wrapper'>
+          <h4 className='form-title'>Qual o seu CEP de cobrança</h4>
+          <div className='form-items'>
+            <label className='form-label'>
+              CEP
+              <InputMask
+                className={`form-input ${(this.props.formError && this.state.addressCep === '') ? `form-input-err` : ``}`}
+                mask='99999-999'
+                onChange={(event) => this.handleCardForm(event, 'addressCep', '')}
+                value={this.state.addressCep}
+              />
+              <span className='form-helper'>Não sei meu CEP</span>
+            </label>
           </div>
-        </label>
+        </div>{/*form wrapper*/}
+
+        {this.state.addressType === 'diferent' &&
+
+          <div className='form-wrapper'>
+            <h4 className='form-title'>Confirme seu endereço de cobrança</h4>
+            <div className='form-items'>
+              <label className='form-label'>
+                Logradouro
+                <input
+                  className={`form-input form-input-addr ${(this.props.formError && this.state.addressType === 'diferent' && this.state.addressAdr === '') ? `form-input-err` : ``}`}
+                  onChange={(e) => this.handleCardForm(e, 'addressAdr', '')}
+                  value={this.state.addressStreet}
+                  type='text'
+                />
+              </label>
+            </div>
+
+            <div className='form-items'>
+              <label className='form-label'>
+                Número
+                <input
+                  className={`form-input form-input-addr-num ${(this.props.formError && this.state.addressType === 'diferent' && this.state.addressNum === '') ? `form-input-err` : ``}`}
+                  onChange={(e) => this.handleCardForm(e, 'addressNum', '')}
+                  value={this.state.addressNum}
+                  type='text'
+                />
+              </label>
+            </div>
+
+            <div className='form-items'>
+              <label className='form-label'>
+                Complemento
+                <input className='form-input form-input-compl' type='text' />
+              </label>
+            </div>
+
+            <div className='form-items'>
+              <label className='form-label'>
+                UF
+                <div
+                  className={`form-select form-select-uf ${(this.props.formError && this.state.addressType === 'diferent' && this.state.addressUF === '') ? `form-select-err` : ``}`}
+                >
+                  <select onChange={(event) => this.handleCardForm(event, 'addressUF', '')}>
+                    <option>--</option>
+                    <option>BA</option>
+                    <option>PB</option>
+                    <option>PE</option>
+                  </select>
+                </div>
+              </label>
+            </div>
+
+            <div className='form-items'>
+              <label className='form-label'>
+                Cidade
+                <div
+                  className={`form-select form-select-city ${(this.props.formError && this.state.addressType === 'diferent' && this.state.addressCity === '') ? `form-select-err` : ``}`}
+                >
+                  <select onChange={(event) => this.handleCard(event, 'addressCity', '')}>
+                    <option>--</option>
+                    <option>Garanhuns</option>
+                    <option>Recife</option>
+                    <option>João Pessoa</option>
+                  </select>
+                </div>
+              </label>
+            </div>
+          </div> //form wrapper
+        }
+
+        <div className='form-wrapper'>
+          <h4 className='form-title'>Dados do cartão</h4>
+
+          <ReactCreditCards
+            number={this.state.ccNum}
+            name={this.state.ccName}
+            expiry={`${this.state.ccYear}/${this.state.ccMonth}`}
+            cvc={this.state.ccCVC}
+            focused={this.state.ccFucused}
+          />
+
+          <div className='form-items'>
+            <label className='form-label'>
+              Número
+              <InputMask
+                className={`form-input form-input-cc-num ${(this.props.formError && this.state.ccNum === '') ? `form-input-err` : ``}`}
+                mask='9999 9999 9999 9999'
+                maskChar=''
+                onChange={(event) => this.handleCardForm(event, 'ccNum', 'number')}
+                value={this.state.ccNumber}
+              />
+            </label>
+          </div>
+
+          <div className='form-items'>
+            <label className='form-label'>
+              Nome
+              <input
+                className={`form-input form-input-cc-name ${(this.props.formError && this.state.ccName === '') ? `form-input-err` : ``}`}
+                onChange={(event) => this.handleCardForm(event, 'ccName', 'name')}
+                value={this.state.ccName}
+              />
+            </label>
+          </div>
+          </div>
+          <div className='form-items'>
+            <label className='form-label'>
+              Validade
+              <div
+                className={`form-select form-select-uf ${(this.props.formError && this.state.ccYear === '') ? `form-select-err` : ``}`}
+              >
+                <select onChange={(e) => this.handleCardForm(e, 'ccYear', 'expiry')}>
+                  <option>--</option>
+                  <option>18</option>
+                  <option>19</option>
+                  <option>20</option>
+                </select>
+              </div>
+            </label>
+          </div>
+
+          <div className='form-items'>
+            <label className='form-label'>
+              <div
+                className={`form-select form-select-uf ${(this.props.formError && this.state.ccMonth === '') ? `form-select-err` : ``}`}
+              >
+                <select onChange={(event) => this.handleCardForm(event, 'ccMonth', 'expiry')}>
+                  <option></option>
+                  <option>01</option>
+                  <option>02</option>
+                  <option>03</option>
+                  <option>04</option>
+                  <option>05</option>
+                  <option>06</option>
+                </select>
+              </div>
+            </label>
+          </div>
+
+          <div className='form-items'>
+            <label className='form-label'>
+              Código de segurança
+              <InputMask
+                className={`form-input form-input-cc-cvc ${(this.props.formError && this.state.ccCVC === '') ? `form-input-err` : ``}`}
+                mask='999'
+                maskChar=''
+                onChange={(event) => this.handleCardForm(event, 'ccCVC', 'cvc')}
+                value={this.state.ccCVC}
+              />
+            </label>
+          </div>
+
+        <div className='form-wrapper'>
+          <h4 className='form-title'>Valor e parcelamento</h4>
+          <label className='form-label'>
+            <div className='form-select form-select-cc-installments'>
+              <select>
+                <option>5x de R$132,00</option>
+                <option>4x de R$165,00</option>
+                <option>3x de R$220,00</option>
+                <option>2x de R$330,00</option>
+                <option>1x de R$660,00</option>
+              </select>
+            </div>
+          </label>
         </div>
+
+        <div className='form-actions'>
+          <div className='form-actions-column form-actions-column--back'>
+            <a className='form-button form-back'>Voltar</a>
+          </div>
+          <div className='form-actions-column form-actions-column--submit'>
+            <button onClick={this.submitForm} className='form-button form-button--active form-submit'>Finalizar compra</button>
+          </div>
         </div>
-        </div>
-      </section>
-    );
+      </div>
+    )
   }
 }
 
