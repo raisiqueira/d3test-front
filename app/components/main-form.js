de8b1df36e0c3146c63b2f3788812833e4e5b10b
Raí Siqueira <rai93siqueira@gmail.com>
2018-01-25 13:41:04 -0300

    add checkout items component to main-form

--- app/components/main-form.js
+++ app/components/main-form.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import Forms from './forms';
+import CheckoutInfo from './checkout-info';
 
 const MainForm = ({ formError, submitFormError }) =>
     <main className="container main">
         <Forms submitErr={formError} submitFormError={submitFormError} />
+        <CheckoutInfo />
     </main>;
 
 export default MainForm;
